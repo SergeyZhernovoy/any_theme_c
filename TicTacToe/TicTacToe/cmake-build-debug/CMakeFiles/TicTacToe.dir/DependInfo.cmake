ccac03eb838fd83b487ab21352f7c1fe2d13e7bf
Sergey Zhernovoy <szhernov@gmail.com>
2018-09-18 16:45:33 +0300

    pointer edu

---
+++ TicTacToe/TicTacToe/cmake-build-debug/CMakeFiles/TicTacToe.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dort/Projects/any_theme_c/TicTacToe/TicTacToe/TicTacToe.cpp" "/home/dort/Projects/any_theme_c/TicTacToe/TicTacToe/cmake-build-debug/CMakeFiles/TicTacToe.dir/TicTacToe.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
