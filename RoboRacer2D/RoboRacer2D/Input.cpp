aeae563ec3a95df01b9e3c4e3176fd45351ebafc
Sergey Zhernovoy <szhernov@gmail.com>
2018-10-09 09:16:14 +0300

    ad input

---
+++ RoboRacer2D/RoboRacer2D/Input.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "Input.h"
+
+Input::Input(const HWND p_hwnd)
+{
+	m_command = Command::CM_STOP;
+	m_hWnd = p_hwnd;
+}
+
+Input::~Input()
+{
+	
+}
+
+void Input::Update(const float p_deltaTime)
+{
+	m_command = Command::CM_STOP;
+	if (KEYDOWN(Key::K_LEFT) || KEYDOWN(Key::K_A))
+	{
+		m_command = Command::CM_LEFT;
+	}
+	if (KEYDOWN(Key::K_RIGHT) || KEYDOWN(Key::K_D))
+	{
+		m_command = Command::CM_RIGHT;
+	}
+	if (KEYDOWN(Key::K_UP) || KEYDOWN(Key::K_W) || KEYDOWN(Key::K_LB))
+	{
+		m_command = Command::CM_UP;
+	}
+	if (KEYDOWN(Key::K_DOWN) || KEYDOWN(Key::K_S) || KEYDOWN(Key::K_RB))
+	{
+		m_command = Command::CM_DOWN;
+	}
+	if (KEYDOWN(Key::K_ESC) || KEYDOWN(Key::K_Q))
+	{
+		m_command = Command::CM_QUIT;
+	}
+}
---
+++ RoboRacer2D/RoboRacer2D/Input.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <Windows.h>
+
+class Input
+{
+public:
+	enum Key
+	{
+		K_ESC = VK_ESCAPE,
+		K_SPACE = VK_SPACE,
+		K_LEFT = VK_LEFT,
+		K_RIGHT = VK_RIGHT,
+		K_UP = VK_UP,
+		K_DOWN = VK_DOWN,
+		K_W = 87,
+		K_A = 65,
+		K_S = 83,
+		K_D = 68,
+		K_Q = 81,
+		K_ENTER = VK_RETURN,
+		K_LB = VK_LBUTTON,
+		K_RB = VK_RBUTTON
+	};
+
+	enum Command
+	{
+		CM_LEFT,
+		CM_RIGHT,
+		CM_STOP,
+		CM_UP,
+		CM_DOWN,
+		CM_QUIT
+	};
+
+	#define KEYDOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x800) ? 1 : 0)
+
+protected:
+	Command m_command;
+	HWND m_hWnd;
+
+public:
+	Input(const HWND p_hwnd);
+	~Input();
+
+	void Update(const float p_deltaTime);
+
+	const Command GetCommand() const { return m_command; }
+
+
+};
