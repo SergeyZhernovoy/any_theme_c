ebe958ea7288b358440a4f848cb0fe6da5a32462
SergeyZhernovoy <s_jernov@mail.ru>
2019-05-07 18:07:44 +0300

    Добавлена игра быки и коровы с одним тестовым словом

---
+++ BullCowGame/BullCowGame/AppMain.cpp
@@ -0,0 +1,118 @@
+﻿/* This is the console executable, that makes use of BullCow class
+ * This acts as the view in a MVC pattern, and its responsible for all0
+ * user interaction. For game logic see the FBullCowGame class.
+ */
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+#pragma once
+using FText = std::string;
+
+
+void PrintIntro();
+FText GetValidGuess();
+void PlayGame();
+bool AskToPlayAgain();
+void PrintGameSummary();
+FBullCowGame BCGame;
+
+int main()
+{
+
+	bool bPlayAgain = false;
+	do
+	{
+		PrintIntro();
+		PlayGame();
+		
+		bPlayAgain = AskToPlayAgain();
+	}
+	while (bPlayAgain);
+	return 0;
+}
+
+void PrintIntro()
+{
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << std::endl;
+	std::cout << "          }   {         ___ " << std::endl;
+	std::cout << "          (o o)        (o o) " << std::endl;
+	std::cout << "   /-------\\ /          \\ /-------\\ " << std::endl;
+	std::cout << "  / | BULL |O            O| COW  | \\ " << std::endl;
+	std::cout << " *  |-,--- |              |------|  * " << std::endl;
+	std::cout << "    ^      ^              ^      ^ " << std::endl;
+	std::cout << "Can you guess the " << BCGame.GetHiddenWordLength();
+	std::cout << " letter isogram I'm thinking of?\n";
+	return;
+}
+
+FText GetValidGuess()
+{
+	EGuessStatus Status = EGuessStatus::Invalid_Status;
+	FText Guess;
+	do
+	{
+		int32 CurrentTry = BCGame.GetCurrentTry();
+		
+		std::cout << "Try " << CurrentTry << " of " << BCGame.GetMaxTries();
+		std::cout << ". Enter your guess : ";
+		getline(std::cin, Guess);
+
+		Status = BCGame.CheckGuessValidity(Guess);
+		switch (Status)
+		{
+		case EGuessStatus::Wrong_Length:
+			std::cout << "Please enter a " << BCGame.GetHiddenWordLength() << " letter word.\n\n";
+			break;
+		case EGuessStatus::Not_Isogram:
+			std::cout << "Please enter a word without repeating letters.\n\n";
+			break;
+		case EGuessStatus::Not_Lowercase:
+			std::cout << "Please enter all lowercase letters.\n\n";
+			break;
+		default:
+			break;
+		}
+	}
+	while (Status != EGuessStatus::Ok);
+	return Guess;
+}
+
+void PlayGame()
+{
+	BCGame.Reset();
+	int32 MaxTries = BCGame.GetMaxTries();
+	while (!BCGame.IsGameWon() && BCGame.GetCurrentTry() <= MaxTries)
+	{
+		FText Guess = GetValidGuess(); 
+		FBullCowCount BullCowCount = BCGame.SubmitValidGuess(Guess);
+		std::cout << "Bulls =  " << BullCowCount.Bulls;
+		std::cout << ". Cows =  " << BullCowCount.Cows << "\n\n";
+	}
+	PrintGameSummary();
+	return;
+}
+
+void PrintGameSummary()
+{
+	if (BCGame.IsGameWon())
+	{
+		std::cout << "WELL DONE... YOU WIN!\n";
+	} 
+	else
+	{
+		std::cout << "Better luck next time!\n";
+	}
+	return;
+}
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again with the same hidden word(y/n) ? ";
+	FText Response = "";
+	getline(std::cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
+
